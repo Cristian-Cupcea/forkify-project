add2563dd820bd65ee9ac438a0822d41b59106f8
Cristian-Cupcea <cristiancupcea@yahoo.com>
2021-05-28 22:10:11 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3591e17a-feb0-45dd-a16b-e9c7f2e77bb9';
+export const MODAL_CLOSE_SEC = 2.5;
